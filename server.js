b37a252627badaf0ac1b51882f3c2980756c3ceb
amr abdelhady abdelghany <amrabdelhady794@gmail.com>
2026-04-17 17:08:32 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -25,7 +25,18 @@ async function createClient(sessionId, sessionName) {
     authStrategy: new LocalAuth({ clientId: sessionId, dataPath: '.wwebjs_auth' }),
     puppeteer: {
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process',
+        '--disable-extensions',
+      ],
     },
   });
 
